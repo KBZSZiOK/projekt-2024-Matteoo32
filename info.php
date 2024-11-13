f1458f1801abb347b60c57ea16f6b8774ee2a105
Matteoo32 <156914364+Matteoo32@users.noreply.github.com>
2024-11-13 13:00:10 +0100

    Add files via upload

--- info.php
+++ info.php
@@ -33,16 +33,7 @@
 
             <br><br>
         </div>
-        </div>
-        <footer>
-        Stronę wykonał Mateusz
-        </footer>
-
-    </div>
-</body>
-</html>
-
-<?php
+        <?php
         $servername = "localhost";
         $username = "root";
         $password = "";
@@ -69,4 +60,65 @@
     
 
 ?>
+
+
+        </div>
+        <div id="animacja"><br><br><br><br>
+        <marquee scrollamount="3" scrolldelay="16"><p id="anim">
+            
+Litwo! Ojczyzno moja! ty jesteś jak zdrowie.
+
+Ile cię trzeba cenić, ten tylko się dowie,
+
+Kto cię stracił. Dziś piękność twą w całej ozdobie
+
+Widzę i opisuję, bo tęsknię po tobie.
+
+ 
+
+Panno Święta, co Jasnej bronisz Częstochowy
+
+I w Ostrej świecisz Bramie! Ty, co gród zamkowy
+
+Nowogródzki ochraniasz z jego wiernym ludem!
+
+Jak mnie dziecko do zdrowia powróciłaś cudem
+
+(Gdy od płaczącej matki pod Twoję opiekę
+
+Ofiarowany, martwą podniosłem powiekę
+
+I zaraz mogłem pieszo do Twych świątyń progu
+
+Iść za wrócone życie podziękować Bogu),
+
+Tak nas powrócisz cudem na Ojczyzny łono.
+
+Tymczasem przenoś moję duszę utęsknioną
+
+Do tych pagórków leśnych, do tych łąk zielonych,
+
+Szeroko nad błękitnym Niemnem rozciągnionych;
+
+Do tych pól malowanych zbożem rozmaitem,
+
+Wyzłacanych pszenicą, posrebrzanych żytem;
+
+Gdzie bursztynowy świerzop, gryka jak śnieg biała,
+
+Gdzie panieńskim rumieńcem dzięcielina pała,
+
+A wszystko przepasane, jakby wstęgą, miedzą
+
+Zieloną, na niej z rzadka ciche grusze siedzą.
+</p>
+</marquee>
+        </div>
+        <footer>
+        Stronę wykonał Mateusz
+        </footer>
+
+    </div>
+</body>
+</html>
 
